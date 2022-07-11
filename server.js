6bc794c354a275a639b744b08a94f1e2f10995fc
ClementFra <franquart.clemnt@gmail.com>
2022-07-11 17:52:38 +0200

    create connection to mongodb with mongoose

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+// Define variables extensions
 const express = require("express");
 const cors = require("cors");
+const mongoose = require(`mongoose`);
 require("dotenv").config();
-
+//Settings corps
 const app = express();
 var corsOptions = {
-    origin: "http://localhost:4200/"
-}
+  origin: "http://localhost:4200/",
+};
+//middleware corps
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to piiquante application." });
+  res.json({ message: "Welcome to piiquante application." });
+});
+// Port listener for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+//mongoose connect
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection to database established ");
+  })
+  .catch((error) => {
+    console.log("Connection failed" + error);
   });
-const PORT= process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}.`);
-})
