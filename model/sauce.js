17d488b05e6e38772969f88149bdc3520511990e
ClementFra <franquart.clemnt@gmail.com>
2022-07-12 01:01:00 +0200

    model for sauce and users

---
+++ model/sauce.js
@@ -0,0 +1,20 @@
+const mongoose = require(`mongoose`);
+
+//model for sauces
+const sauceSchema = mongoose.Schema({
+    userId : {type: String , ref: `User`, required: true},
+    name: {type: String, required: true},
+    manufacturer:{type: String, required: true},
+    description:{type: String, required: true},
+    mainPepper:{ type: String, required: true},
+    imageUrl:{type: String, required: true},
+    heat:{type: Number, required: true, min: 1, max: 10},
+    likes:{type: Number, default: 0},
+    dislikes:{type: String, default: 0},
+    usersLiked:{type: String, ref: `User`},
+    usersDisliked:{type: String, ref:`User`},
+    reports:{type: Number, default: 0},
+    usersWhoReported:{type: Number, ref: `User`}
+});
+
+module.exports = mongoose.model(`Sauce`, sauceSchema);
