f64fc1701ae2e37cd9edff58322706ef9b9b3622
ClementFra <franquart.clemnt@gmail.com>
2022-07-12 10:57:43 +0200

    refactor database connection

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Define variables extensions
 const express = require("express");
 const cors = require("cors");
-const mongoose = require(`mongoose`);
+require("./config/db.config");
 require("dotenv").config();
+
 //Settings corps
 const app = express();
 var corsOptions = {
   origin: "http://localhost:4200/",
 };
+
 //middleware corps
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -15,21 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to piiquante application." });
 });
+
 // Port listener for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-//mongoose connect
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connection to database established ");
-  })
-  .catch((error) => {
-    console.log("Connection failed" + error);
-  });
+
