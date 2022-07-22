52f0835a6b35ee18c3a0647b6c69eb217be09ac4
ClementFra <franquart.clemnt@gmail.com>
2022-07-22 11:07:14 +0200

    user data , userupdate initialisation route

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
+const auth = require('../middleware/auth');
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
+router.get(`/`, auth,userCtrl.readUser);
+router.get('/export', auth, userCtrl.exportUserData);
+router.put('/', auth, userCtrl.updateUser);
 
 module.exports = router;
 
