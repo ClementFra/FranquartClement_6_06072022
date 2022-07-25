c72d2a1ef093c3635f2c31301a102a0036b4b4d5
ClementFra <franquart.clemnt@gmail.com>
2022-07-25 23:24:00 +0200

    sauce like & route like dislike

--- app/routes/sauce.js
+++ app/routes/sauce.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 router.get("/:id", auth, sauceCtrl.readSingleSauce);
 router.get("/", auth, sauceCtrl.readAllSauces);
+
+router.post("/", auth, multer, sauceCtrl.createNewSauce);
+router.post('/:id/like', auth, sauceCtrl.likeOrDislike);
 
 module.exports = router;
