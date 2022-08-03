eadf9c96eef7fa7f15c44b7c66c761129087a3f4
ClementFra <franquart.clemnt@gmail.com>
2022-08-03 15:19:36 +0200

    correction users

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -60,27 +60,27 @@ exports.signup = (req, res, next) => {
 
 // Login user who's is already register
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  const encryptedEmail = encrypt(req.body.email);
+  User.findOne({ email: encryptedEmail })
     .then((user) => {
       if (!user) {
         return res.status(401).json({ message: "User not found !" });
       }
+      user.email = decrypt(user.email);
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
             return res.status(401).json({ message: "Incorrect password !" });
           }
-          res.status(200).json(
-            {
-              userId: user._id,
-              // Creating a token for the session
-              token: jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-                expiresIn: "24h",
-              }),
-            },
-            hateoasLinks(req, user._id)
-          );
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+              expiresIn: "24h",
+            }),
+            user: user,
+          });
+          hateoasLinks(req, user._id);
         })
         .catch((error) => res.status(500).json({ error }));
     })
